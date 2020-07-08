18d8dced021f3902b5200c585ab3a9d15d342ba8
Vendetta <34066247+vendettadelsangue@users.noreply.github.com>
2020-07-08 09:22:57 +0900

    Create Print.java

---
+++ 1/Print.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+ class Main
+ {
+   public static void test() {
+     System.out.println("World");
+   }
+   public static void main(String args[]){
+     System.out.println("Hello,");
+     test();
+   }
+ }
+    
